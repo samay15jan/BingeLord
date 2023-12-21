1452447958ac94008e1810d11d3f9bd2ba1b9799
samay15jan <samay15jan@gmail.com>
2023-12-22 04:00:11 +0530

    Added React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import HomePage from './pages/HomePage'
+import ContentPage from './pages/ContentPage'
+import Overflow from './components/contentpage/overflowMenu/Overflow';
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 
 function App() {
+
+  const ScrollToTop = () => {
+    const { pathname } = useLocation();
+  
+    useEffect(() => {
+      window.scrollTo(0, 0);
+    }, [pathname]);
+  
+    return null;
+  };
+  
   return (
-    <>
-      <HomePage />
-    </>
+    <BrowserRouter>
+      <ScrollToTop />
+      <Routes>
+        <Route index element={<HomePage />}/>
+        <Route path="/:type/:id" element={<ContentPage />}/>
+        <Route path="/:type/:id/:menu/:url" element={<Overflow />}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
